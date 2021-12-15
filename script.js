8a47b2882ac4ddcbc277588cf84e8e30f4b673d6
Nayan Agarwal <nayan.agg13@gmail.com>
2021-12-15 17:25:35 +0530

    Added deletion

--- script.js
+++ script.js
@@ -2,31 +2,44 @@
 let btn = document.getElementById('btn');
 btn.addEventListener('click',createTask);
 var ctr = 1;
-async function createTask(event){
+function createTask(event){
     event.preventDefault();
     let content = document.getElementById('task');
     // console.log(content.value);
     if(content.value.length > 0){
 
         let newItem = document.createElement("li");
+        newItem.id = ctr;
         let str = document.createTextNode(content.value);
         newItem.appendChild(str);
+
+        let del = document.createElement('button');
+        del.type = "submit";
+        del.innerText="Delete";
+        del.onclick = (event)=>{
+            //console.log(event.path[1].id);
+            let item = document.getElementById(event.path[1].id);
+            item.remove();
+        }
+
+        let ele = document.createElement('input');
+        ele.type = "checkbox";
+        newItem.appendChild(ele);
+        newItem.appendChild(del);
+
         let list = document.querySelector('#list ul');
         let all = document.querySelectorAll('#list ul li')
-        await list.appendChild(newItem);
+        list.appendChild(newItem);
 
         let count = document.getElementById('ctr');
-        // if(all.length >){
-            //console.log()
-            ctr = all.length;
-        // }else{
-        //     ctr = 1;
-        // }
-        count.innerText = ctr + 1  ;
-        console.log(all);
+        ctr = all.length;
+
+        count.innerText = ctr + 1;
+        //console.log(all);
 
     }else{
         alert("Empty task can't be added!!");
     }
 }
+
 
